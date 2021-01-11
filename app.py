dea09e08dac4750611e74795113f921933ad3151
Jack Parsons <jackpar1113@gmail.com>
2021-01-11 15:18:14 -0700

    implement selection sort

--- app.py
+++ app.py
@@ -46,8 +46,9 @@ class App:
       self.speed_scale = Scale(self.UI_frame_right, from_=0.01, to=1.0, length=170, digits=3, resolution=0.01, orient=HORIZONTAL, label='Speed (Lower is Faster)')
       self.size_entry = Scale(self.UI_frame_right, from_=3, to=50, length=170, resolution=1, orient=HORIZONTAL, label='Size', command=self.generate)
 
-      self.algorithm_functions = {'Merge Sort': lambda:mergeSort(self.data, self.draw, self.speed_scale.get()),
-                                  'Bubble Sort': lambda:bubbleSort(self.data, self.draw, self.speed_scale.get())}
+      self.algorithm_functions = {'Bubble Sort': lambda:bubbleSort(self.data, self.draw, self.speed_scale.get()),
+                                  'Merge Sort': lambda:mergeSort(self.data, self.draw, self.speed_scale.get()),
+                                  'Selection Sort': lambda:selectionSort(self.data, self.draw, self.speed_scale.get())}
       
    def start(self):
       self.generate() # Display data when app starts
---
+++ selectionSort.py
@@ -0,0 +1,12 @@
+import time
+
+def selectionSort(data, draw, speed):
+   for i in range(len(data)):
+      min_index = i
+      for j in range(i+1, len(data)):
+         if data[j] < data[min_index]:
+            min_index = j
+
+      draw(data, ['#2EC4B6' if x == i or x == min_index else '#EDF2F4' for x in range(len(data))])
+      time.sleep(speed)
+      data[i], data[min_index] = data[min_index], data[i]
