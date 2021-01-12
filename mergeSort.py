15eb92dfb285383c4e4b1fcd7a6262798e04e30f
Jack Parsons <jackpar1113@gmail.com>
2021-01-11 17:03:01 -0700

    update merge sort

--- mergeSort.py
+++ mergeSort.py
@@ -12,7 +12,7 @@ def _mergeSort(data, left, right, draw, speed):
       merge(data, left, middle, right, draw, speed)
 
 def merge(data, left, middle, right, draw, speed):
-   draw(data, colorArray(len(data), left, middle, right))
+   draw(data, ["#2EC4B6" if x >= left and x <= right else "#EDF2F4" for x in range(len(data))])
    time.sleep(speed)
 
    left_partition = data[left:middle+1]
@@ -38,17 +38,3 @@ def merge(data, left, middle, right, draw, speed):
    
    draw(data, ["#2EC4B6" if x >= left and x <= right else "#EDF2F4" for x in range(len(data))])
    time.sleep(speed)
-
-def colorArray(length, left, middle, right):
-   colorArray = []
-
-   for i in range(length):
-      if i >= left and i <= right:
-         if i >= left and i <= middle:
-               colorArray.append("#FF9F1C")
-         else:
-               colorArray.append("#EE6C4D")
-      else:
-         colorArray.append("#EDF2F4")
-
-   return colorArray
